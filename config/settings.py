f889007ae62e6299a98d472200d85db3c907d93c
mihail <mihail.bashkatov@gmail.com>
2025-05-02 18:43:29 +0200

    #

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         # "HOST": os.getenv("POSTGRES_HOST"),
-        "HOST": "db"),
+        "HOST": "db",
         "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
